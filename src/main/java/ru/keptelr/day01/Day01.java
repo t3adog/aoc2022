1f8f00fea1404765f334019210e9aac662d8ec7f
Petr Belyakov <keptelr@ya.ru>
2022-12-01 22:39:01 +0300

    day01 part01 completed

--- src/main/java/ru/keptelr/day01/Day01.java
+++ src/main/java/ru/keptelr/day01/Day01.java
@@ -1,7 +1,34 @@
 package ru.keptelr.day01;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Day01 {
-    public static void main(String[] args) {
-        //TODO
+
+    private List<Elve> parseElves(List<String> input) {
+        List<Elve> elves = new ArrayList<>();
+
+        Elve tmpElve = new Elve();
+        for(String str : input) {
+            if (str.length() == 0) {
+                elves.add(tmpElve);
+                tmpElve = new Elve();
+            } else {
+                tmpElve.setCalories(tmpElve.getCalories() + Long.parseLong(str));
+            }
+        }
+
+        return elves;
     }
+
+    public Long partOne(List<String> input) {
+        List<Elve> elves = parseElves(input);
+        return elves.stream().map(Elve::getCalories).max(Comparator.naturalOrder()).get();
+    }
+
+    public Long partTwo(List<String> input) {
+        return 0L;
+    }
+
 }
--- src/test/java/ru/keptelr/day01/Day01Test.java
+++ src/test/java/ru/keptelr/day01/Day01Test.java
@@ -1,5 +1,25 @@
 package ru.keptelr.day01;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.keptelr.utils.InputParserUtil;
+
 public class Day01Test {
+
+    @Test
+    public void testPartOne() {
+
+        List<String> input = InputParserUtil.readFile("day01");
+        Day01 day01 = new Day01();
+        Assert.assertEquals(24000L, day01.partOne(input), 0);
+    }
+
+    @Test
+    public void testPartTwo() {
+        
+    }
     //TODO
 }
