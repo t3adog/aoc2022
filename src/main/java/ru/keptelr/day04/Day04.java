f3f0e19fbcaad00b0cada7e846d3b05305bdfcd5
Petr Belyakov <keptelr@ya.ru>
2022-12-04 15:25:26 +0300

    init day04

---
+++ src/main/java/ru/keptelr/day04/Day04.java
@@ -0,0 +1,14 @@
+package ru.keptelr.day04;
+
+import java.util.List;
+
+public class Day04 {
+    public int partOne(List<String> input) {
+       return 0;
+    }
+
+    public int partTwo(List<String> input) {
+       return 0;
+    }
+
+}
---
+++ src/test/java/ru/keptelr/day04/Day04Test.java
@@ -0,0 +1,24 @@
+package ru.keptelr.day04;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.keptelr.utils.InputParserUtil;
+
+public class Day04Test {
+    @Test
+    public void testPartOne() {
+        List<String> input = InputParserUtil.readFile("day04");
+        Day04 day04 = new Day04();
+        Assert.assertEquals(0, day04.partOne(input));
+    }
+
+    @Test
+    public void testPartTwo() {
+        List<String> input = InputParserUtil.readFile("day04");
+        Day04 day04 = new Day04();
+        Assert.assertEquals(0, day04.partTwo(input));
+    }
+}
