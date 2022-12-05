8563140c429605cc5f8396e551372c44625146a4
Petr Belyakov <keptelr@ya.ru>
2022-12-05 12:16:01 +0300

    day05 - initial commit

---
+++ src/main/java/ru/keptelr/day05/Day05.java
@@ -0,0 +1,17 @@
+package ru.keptelr.day05;
+
+import java.util.List;
+
+public class Day05 {
+
+    public int partOne(List<String> input) {
+
+       return 0;
+    }
+
+    public int partTwo(List<String> input) {
+
+        return 0;
+    }
+
+}
