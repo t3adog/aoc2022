f4f17ca147c6242def004c5deb6536ace2dc619c
Petr Belyakov <pabelyak@mts.ru>
2022-12-06 08:51:41 +0300

    day06 - part01 completed

--- src/main/java/ru/keptelr/day06/Day06.java
+++ src/main/java/ru/keptelr/day06/Day06.java
@@ -1,10 +1,26 @@
 package ru.keptelr.day06;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day06 {
 
+    private static final int OFFSET = 4;
+
     public int partOne(List<String> input) {
+        char[] source = input.get(0).toCharArray();
+
+        for (int i = 0; i < source.length; i++) {
+            Set<Character> buffer = new HashSet<>();
+            for (int z = 0; z < OFFSET; z++) {
+                buffer.add(source[i + z]);
+            }
+            if (buffer.size() == OFFSET) {
+                return i + OFFSET;
+            }
+
+        }
         return 0;
     }
 
--- src/test/java/ru/keptelr/day06/Day06Test.java
+++ src/test/java/ru/keptelr/day06/Day06Test.java
@@ -1,5 +1,7 @@
 package ru.keptelr.day06;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,12 +11,16 @@ public class Day06Test {
     @Test
     public void testPartOne() {
         Day06 day06 = new Day06();
-        Assert.assertEquals(0, day06.partOne(InputParserUtil.readFile("day06")));
+        Assert.assertEquals(7, day06.partOne(List.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb")));
+        Assert.assertEquals(5, day06.partOne(List.of("bvwbjplbgvbhsrlpgdmjqwftvncz")));
+        Assert.assertEquals(6, day06.partOne(List.of("nppdvjthqldpwncqszvftbrmjlhg")));
+        Assert.assertEquals(10, day06.partOne(List.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")));
+        Assert.assertEquals(11, day06.partOne(List.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")));
     }
 
     @Test
     public void testPartTwo() {
         Day06 day06 = new Day06();
-        Assert.assertEquals(0, day06.partTwo(InputParserUtil.readFile("day06")));
+        // Assert.assertEquals(0, day06.partTwo(InputParserUtil.readFile("day06")));
     }
 }
