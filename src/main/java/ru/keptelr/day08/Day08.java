74ee086e8978f18f7b2db91fd1dbdde872a28360
Petr Belyakov <keptelr@ya.ru>
2022-12-08 02:24:21 +0300

    day08 initial commit

---
+++ src/main/java/ru/keptelr/day08/Day08.java
@@ -0,0 +1,14 @@
+package ru.keptelr.day08;
+import java.util.List;
+
+public class Day08 {
+
+    public Integer partOne(List<String> input) {
+        return 0;
+    }
+
+    public int partTwo(List<String> input) {
+        return 0;
+    }
+
+}
