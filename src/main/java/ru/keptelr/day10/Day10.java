6118c4e81c802923a7fd098272acb052f927bb40
Petr Belyakov <keptelr@ya.ru>
2022-12-11 12:35:32 +0300

    day10 initial commit

---
+++ src/main/java/ru/keptelr/day10/Day10.java
@@ -0,0 +1,16 @@
+package ru.keptelr.day10;
+
+import java.util.List;
+
+public class Day10 {
+
+    public Integer partOne(List<String> input) {
+        return 0;
+    }
+
+
+    public int partTwo(List<String> input) {
+        return 0;
+    }
+
+}
