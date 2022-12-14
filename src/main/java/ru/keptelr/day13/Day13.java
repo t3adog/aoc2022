be3e6173f52201bd219ade5b318e41e50ac99bd7
Petr Belyakov <keptelr@ya.ru>
2022-12-14 20:56:25 +0300

    day13 parsing finished

--- src/main/java/ru/keptelr/day13/Day13.java
+++ src/main/java/ru/keptelr/day13/Day13.java
@@ -1,9 +1,34 @@
 package ru.keptelr.day13;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day13 {
+
+    private List<Pair<Package, Package>> parsePackages(List<String> input) {
+        List<Pair<Package, Package>> packages = new ArrayList<>();
+        for (int i = 0; i < input.size(); i = i + 3) {
+            if (input.get(i).startsWith("[") && input.get(i + 1).startsWith("[")) {
+                String leftPackageStr = input.get(i).substring(1, input.get(i).length() - 1);
+                String rightPackageStr = input.get(i+1).substring(1, input.get(i+1).length() - 1);
+                packages.add(Pair.of(Package.from(leftPackageStr), Package.from(rightPackageStr)));
+            } else {
+                throw new RuntimeException("");
+            }
+        }
+        return packages;
+    }
+
     public Integer partOne(List<String> input) {
+        List<Pair<Package, Package>> parsePackages = parsePackages(input);
+
+        for (Pair<Package, Package> pair : parsePackages) {
+            System.out.println("Левый " + pair.getLeft());
+            System.out.println("Правый " + pair.getRight());
+            System.out.println("~~~~~");
+        }
         return 0;
     }
 
--- src/test/java/ru/keptelr/day13/Day13Test.java
+++ src/test/java/ru/keptelr/day13/Day13Test.java
@@ -2,9 +2,15 @@ package ru.keptelr.day13;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 import ru.keptelr.utils.InputParserUtil;
 
 public class Day13Test extends TestCase {
+
+    @Test
+    public void testParsing() {
+        System.out.println(Package.from("[[1],[2,3,4]]"));
+    }
 
     public void testPartOne() {
         Day13 day13 = new Day13();
